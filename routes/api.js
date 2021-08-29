4bec5251837215f9a397fdd5cba6823825dac49c
linz-M <89681639+linz-M@users.noreply.github.com>
2021-08-29 09:39:50 +0700

    api.js

--- routes/api.js
+++ routes/api.js
@@ -24,10 +24,12 @@ var TikTokScraper = require('tiktok-scraper');
 var router  = express.Router();
 var bodyParser = require('body-parser');
 
-var nhentai = require('nhentai-js');
-var fs = require('fs');
-var request = require('request');
-var imgToPDF = require('image-to-pdf');
+var fs = require('fs')
+var axios = require('axios')
+var request = require('request')
+var topdf = require('image-to-pdf')
+var nhentai = require('nhentai-node-api')
+
 var path = require('path');
 var nHentaiAPI = require('nhentai-api-js');
 let api = new nHentaiAPI();
@@ -173,84 +175,6 @@ var len = 15
         var randomTextNumber = random+randomlagi+'---------ZahirGanteng'+'ZHIRRR--GANS';
         
  
- async function getDoujin(id) {
-
-  const PDFpages = []; //name of pages will be stored here, later to smash it all together to make PDF
-  const directory = 'temp_images'; // name of the directory where temp_image files will be stored to make PDF
-  let pages_array = [];
-
-  try {
-
-    console.log('\x1b[41m%s\x1b[0m', 
-      `Welcome To 『 KODE NUKLIR TO PDF :V 』`);
-    console.log('\x1b[41m%s\x1b[0m', 
-      `        『 Created By Somnath Das, @samurai3247 [Instagram] 』`);
-    console.log('\x1b[44m%s\x1b[0m', 
-      `Processing and Converting your Code, Please Wait, senpai uwu`);
-
-    // To Create Directory named "temp_images"
-    fs.mkdir("temp_images", (damn_error) => {
-            if(damn_error) {
-              if(damn_error.hasOwnProperty('errno') && damn_error['errno'] == '-17') {
-                console.log("Directory already exists");
-              } else {
-              console.log(damn_error);
-              }
-            } else if(!damn_error){
-              console.log("Created New Directory To Store Images");
-            }
-          });
-
-    if(nhentai.exists(id)) { // Checks if Doujin exists
-
-        const dojin = await nhentai.getDoujin(id);
-        pages_array = dojin.pages;
-        let title = dojin.title;
-        let download_count = 0;
-
-      // pages_array directly refers to an array of links of the images of the pages.
-        pages_array = dojin.pages;
-
-      // To download images from the direct url present in pages_array
-        console.log(`Doujin title: ${title}`);
-        console.log("Downloading...")
-        for (let i = 0; i < pages_array.length; i++) {
-          image_name = 'temp_images/image' + i + '.jpg';
-          await new Promise((resolve) => request(pages_array[i]).pipe(fs.createWriteStream(image_name)).on('finish', resolve))
-          PDFpages.push(image_name);
-          download_count++;
-          console.log(`Downloading: ${download_count} out of ${pages_array.length}`)
-        }
-
-    // To convert images into pdf file using an API named "image-to-pdf"
-        imgToPDF(PDFpages, 'A4').pipe(fs.createWriteStream(title + '.pdf'));
-
-    // To Read the temp_images directory, collect the name of files in there and delete image files after using them
-        try {
-          fs.readdir(directory, (err, files) => {
-          if (err) throw err;
-
-          for (const file of files) {
-            fs.unlink(path.join(directory, file), err => {
-            if (err) throw err;
-            });
-          }
-          });
-        }catch(eRR) {
-          console.log(eRR);
-        }
-    } else { // Responds if doujin doesn't exists
-      console.log("Nuke Code doesn't exists, bakka shi nee *^*")
-    }
-  }catch(err) {
-    console.log(err);
-  }finally {
-    console.log("Completed");        
-  }
-}
- 
- 
- 
  
  
  
@@ -387,15 +311,53 @@ router.get('/remove', (req, res, next) => {
     }
 })
 
-router.get('/nhentai/pdf', async (req, res, next) => {
-      var code = req.query.code            
-	  if(!code) return res.send({"status": "error", "message": "missing parameter code"});
-  	let doujin_id = code;
-      getDoujin(doujin_id);
-      const diojin = await nhentai.getDoujin(doujin_id);
-      let judul = diojin.title;
-      return res.sendFile(__path+ `/${judul}.pdf`)
-});
+router.get('/nhentai/pdf', async (req, respon, next) => {
+        var code = req.query.code
+        if(!code) return respon.send({"status": "error", "message": "missing parameter code"});
+		let count = 0
+		let ResultPdf = []
+		let doujin = await nhentai.getDoujin(input)
+		let array_page = doujin.pages
+		let title = doujin.titles.pretty
+
+		for (let index = 0; index < array_page.length; index++) {
+			let image_name = './nhentai/' + title + index + '.jpg'
+			await new Promise((resolve) => request(array_page[index]).pipe(fs.createWriteStream(image_name)).on('finish', resolve))
+			console.log(array_page[index])
+			ResultPdf.push(image_name)
+			count++
+		}
+
+		await new Promise((resolve) =>
+			topdf(ResultPdf, 'A4')
+			.pipe(fs.createWriteStream('./nhentai/' + title + '.pdf'))
+			.on('finish', resolve)
+		)
+			let options = {
+				method: 'POST',
+				url: 'https://api.anonfiles.com/upload',
+				formData: {
+					file: fs.createReadStream(`./nhentai/${title}.pdf`),
+				},
+			}
+
+			for (let index = 0; index < array_page.length; index++) {
+				fs.unlink('./nhentai/' + title + index + '.jpg', (err) => {
+					if (err) throw err
+				})
+			}
+			
+			request(options, function(err, res, body) {
+				if (err) console.log(err)
+				fs.unlink(`./nhentai/${title}.pdf`, (err) => {
+					if (err) throw err
+				})
+				return respon.json(body);
+			})
+		}
+	}
+}
+
 
 
 router.get('/nhentai/search', async (req, res, next) => {
